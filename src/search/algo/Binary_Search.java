766f952173e057aebf36fe9814cfa6226decc5d5
Ravi Mulani <ravimulani101@gmail.com>
2023-09-10 11:03:52 -0400

    add recursive function for binary search

--- src/search/algo/Binary_Search.java
+++ src/search/algo/Binary_Search.java
@@ -4,9 +4,15 @@ public class Binary_Search {
 	public static void main(String[] args) {
 
 		int a[] = {1, 3, 5, 6, 8, 9, 15};
-		int target = 1;
+		int target = 6;
+		
+//		Helper variable for recursive binary search
+//		int left = 0;
+//		int right = a.length -1;
 
 		int result = binarySearch(a, target);
+		//Recursive binary search
+//		int result = recursiveBinarySearch(a, target, left, right);
 
 		if (result != -1) {
 			System.out.println("Target Value Found At Index: " + result);}
@@ -29,6 +35,25 @@ public class Binary_Search {
 			}
 			else {
 				right = mid - 1;
+			}
+		}
+		return -1;
+	}
+	
+	// Helper function for recursive binary search
+	public static int recursiveBinarySearch(int[] a, int target, int left, int right) {
+		while(left<=right) {	
+			int mid = (left + right)/2;
+			if(a[mid] == target) {
+				return mid;
+			}
+			
+			else if(a[mid] < target) {
+				return recursiveBinarySearch(a, target, mid + 1, right);
+			}
+			
+			else {
+				return recursiveBinarySearch(a, target, left, mid - 1);
 			}
 		}
 		return -1;
