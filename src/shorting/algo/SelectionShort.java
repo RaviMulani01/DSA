fda1a6d1a69215d5674a4c01aba13914288b031f
Ravi Mulani <ravimulani101@gmail.com>
2023-09-12 21:37:46 -0400

    selection sort

---
+++ src/shorting/algo/SelectionShort.java
@@ -0,0 +1,34 @@
+package shorting.algo;
+
+public class SelectionShort {
+	   public static void main(String args[]) {
+	       int arr[] = {5, 12, 2, 4, 1};
+	       selectionsort(arr);
+	       printArray(arr);
+	   }
+	   
+		public static void selectionsort(int[] arr) {
+			//selection sort
+		       for(int i=0; i<arr.length-1; i++) {
+		           int smallest = i;
+		           for(int j=i+1; j<arr.length; j++) {
+		               if(arr[j] < arr[smallest]) {
+		                   smallest = j;
+		               }
+		           }
+		           //swap
+		           int temp = arr[smallest];
+		           arr[smallest] = arr[i];
+		           arr[i] = temp;
+		       }
+		}
+		
+		//print array
+		public static void printArray(int arr[]) {
+		       for(int i=0; i<arr.length; i++) {
+		           System.out.print(arr[i]+" ");
+		       }
+		       System.out.println();
+		   }
+
+}
