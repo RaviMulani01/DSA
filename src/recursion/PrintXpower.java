a6aab17ea0541ba615c10f2e2b4b7e598f2a07e7
Ravi Mulani <ravimulani101@gmail.com>
2023-09-20 14:49:51 -0400

    Print x^n (with stack height = logn)

--- src/recursion/PrintXpower.java
+++ src/recursion/PrintXpower.java
@@ -4,9 +4,9 @@ public class PrintXpower {
 
 	public static void main(String[] args) {
 		int x = 2, n = 5;
-		int output = printPowerwithhightN(x, n);
+//		int output = printPowerwithhightN(x, n);
+		int output = printPowerwithhightlogN(x, n);
 		System.out.println(output);
-
 	}
 
 	//Print x^n (with stack height = n)
@@ -21,6 +21,26 @@ public class PrintXpower {
 		
 		int result = x * printPowerwithhightN(x, n-1);
 		return result;
+	}
+
+	//Print x^n (with stack height = logn)
+	private static int printPowerwithhightlogN(int x, int n) {
+		if(x == 0) {
+			return 0;
+		}
+		
+		if (n == 0) {
+			return 1;
+		}
+		
+		if(n%2 == 0) {// for even power
+			return  printPowerwithhightlogN(x, n/2) * printPowerwithhightlogN(x, n/2);
+		}
+		
+		else {// for odd power
+			return printPowerwithhightlogN(x, n/2) * printPowerwithhightlogN(x, n/2) * x;
+		}
+		
 	}
 
 }
